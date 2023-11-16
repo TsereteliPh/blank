64d8c8058afbd0e1e3261e35412e9039b5c50bcc
TsereteliPh <filipptsereteli@yandex.ru>
2023-11-16 11:08:23 +0300

    add and done .image-text

---
+++ layouts/blocks/image-text/template.php
@@ -0,0 +1,16 @@
+<section class="image-text">
+	<div class="container image-text__container">
+		<?php get_template_part('/layouts/partials/title', null, array(
+			'class' => 'faq__title',
+			'title' => get_sub_field('title')
+		)); ?>
+
+		<?php if ( get_sub_field( 'text' ) ) : ?>
+			<div class="image-text__text"><?php the_sub_field( 'text' ); ?></div>
+		<?php endif; ?>
+	</div>
+
+	<?php if ( get_sub_field( 'img' ) ) : ?>
+		<div class="image-text__image" style="background-image: url('<?php the_sub_field( 'img' ); ?>');"></div>
+	<?php endif; ?>
+</section>
